fac3b80668efebc9e3298483ff25ffc8d8ec3f84
Riam97 <mariajosemolina.mjmc@gmail.com>
2024-02-14 23:34:59 -0300

    avance

---
+++ index.js
@@ -0,0 +1,26 @@
+const profileElement = document.querySelector(".profile");
+const profileNameElement = profileElement.querySelector(".profile__name");
+const profileOcupationElement = profileElement.querySelector(
+  ".profile__ocupation"
+);
+
+const profileName = profileNameElement.textContent;
+const profileOcupation = profileOcupationElement.textContent;
+
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editPopupElement = document.querySelector(".popup");
+
+const formElement = document.querySelector(".popup__form");
+const nameForm = formElement.querySelector(".popup__form-name");
+const ocupationForm = formElement.querySelector("popup__form-ocupation");
+
+function setPopupInputs() {
+  nameForm.value = profileName;
+  ocupationForm.value = profileOcupation;
+}
+
+function openPopup() {
+  editPopupElement.classList.toggle("popup_opened");
+}
+
+editProfileButton.addEventListener("click", openPopup);
