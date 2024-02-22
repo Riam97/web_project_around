d79dd453b306db4e6da943196c488bd098e2984e
Riam97 <mariajosemolina.mjmc@gmail.com>
2024-02-21 21:35:11 -0300

    boton add card funcionando

--- index.js
+++ index.js
@@ -5,33 +5,52 @@ const profileOcupationElement = profileElement.querySelector(
 );
 
 const editProfileButton = document.querySelector(".profile__edit-button");
-const editPopupElement = document.querySelector(".popup");
+const editPopupElement = document.querySelector(".popup__profile");
 
 const formElement = document.querySelector(".popup__form");
-const nameForm = formElement.querySelector(".popup__form-name");
-const ocupationForm = formElement.querySelector(".popup__form-ocupation");
 
-const button = formElement.querySelector(".popup__button");
+const buttonPopupForm = formElement.querySelector(".popup__button");
 const popupName = formElement.querySelector(".popup__name");
 const popupOcupation = formElement.querySelector(".popup__ocupation");
+
+const addCardElement = document.querySelector(".cards");
+const cardTitleElement = addCardElement.querySelector(".card__title");
+const cardImageElement = addCardElement.querySelector(".card__image");
+
+const addCardButton = document.querySelector(".profile__button-add-card");
+const addCardPopupElement = document.querySelector(".popup__add-card");
+
+const cardTitle = addCardPopupElement.querySelector(".popup__card-title");
+const cardImage = addCardPopupElement.querySelector(".popup__card-image");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileNameElement.textContent = popupName.value;
   profileOcupationElement.textContent = popupOcupation.value;
-  /*editPopupElement.classList.remove("popup__opened");*/
   closePopup();
 }
 
-function openPopup() {
+function openProfilePopup() {
   editPopupElement.classList.add("popup__opened");
   popupName.value = profileNameElement.textContent;
   popupOcupation.value = profileOcupationElement.textContent;
 }
 
+function openAddCardPopup() {
+  addCardPopupElement.classList.add("popup__opened");
+  cardTitle.value = cardTitleElement.textContent;
+  cardImage.value = cardImageElement.textContent;
+}
+
 function closePopup() {
-  editPopupElement.classList.remove("popup__opened");
+  if (editPopupElement.classList.contains("popup__opened")) {
+    editPopupElement.classList.remove("popup__opened");
+  }
+  if (addCardPopupElement.classList.contains("popup__opened")) {
+    addCardPopupElement.classList.remove("popup__opened");
+  }
 }
 
 formElement.addEventListener("submit", handleFormSubmit);
-editProfileButton.addEventListener("click", openPopup);
+editProfileButton.addEventListener("click", openProfilePopup);
+addCardButton.addEventListener("click", openAddCardPopup);
