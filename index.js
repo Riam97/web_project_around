0db40a87c6b7e37d5da3225ce8e3eaffbcb8daec
Riam97 <mariajosemolina.mjmc@gmail.com>
2024-03-30 21:45:09 -0300

    formularios validados

--- index.js
+++ index.js
@@ -139,3 +139,76 @@ function openImagePopup(src, title) {
   cardTitlePopup.alt = title;
   cardTitlePopup.textContent = title;
 }
+
+//desde aqui se han hecho cambios
+const forms = document.querySelectorAll('form[name="form"]');
+
+const showInputError = (formElement, formInput, errorMessage) => {
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+
+  formInput.classList.add("popup__input_error");
+  formError.textContent = errorMessage;
+  formError.classList.add("popup__input-error_active");
+};
+
+const hideInputError = (formElement, formInput) => {
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+
+  formInput.classList.remove("popup__input_error");
+  formError.classList.remove("popup__input-error_active");
+  formError.textContent = "";
+};
+
+const isValid = (formElement, formInput) => {
+  if (!formInput.validity.valid) {
+    showInputError(formElement, formInput, formInput.validationMessage);
+  } else {
+    hideInputError(formElement, formInput);
+  }
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("popup__button_inactive");
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove("popup__button_inactive");
+    buttonElement.disabled = false;
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+
+  const buttonElement = formElement.querySelector(".popup__button");
+
+  inputList.forEach((formInput) => {
+    formInput.addEventListener("input", () => {
+      isValid(formElement, formInput);
+
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return Array.from(inputList).some((formInput) => {
+    return !formInput.validity.valid;
+  });
+};
+
+const enableValidation = () => {
+  forms.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      const formInputs = formElement.querySelectorAll(".popup__input");
+      if (hasInvalidInput(formInputs)) {
+        evt.preventDefault();
+      }
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+
+//formInput.addEventListener("Input se ejecuta", isValid);//
